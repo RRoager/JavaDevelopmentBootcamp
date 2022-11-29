098865c70c0068be2c80d0399554880be71deddb
RRoager <rasmus-roager@hotmail.com>
2022-11-29 17:00:00 +0100

    Debugging finished and ExceptionHandleling started

---
+++ Section18-ExceptionHandlingAndDebugging/src/main/java/org/example/Pyramid1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Pyramid1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == i) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
